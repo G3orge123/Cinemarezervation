0338708ca38981111cc835c193aa76d420af10e4
George <11c.george.ilie@zinca-golescu.ro>
2025-01-20 17:25:12 +0200

    Updated schema.sql with corrected database structure

--- database/schema.sql
+++ database/schema.sql
@@ -2,12 +2,11 @@ CREATE DATABASE if NOT EXISTS db_cinema;
 USE db_cinema;
 
 CREATE TABLE if not exists reservations(
-    id INT AUTO_INCREMENT primary key;
-    customer_name VARCHAR(100) not null;
-    movie_name VARCHAR(100) not null;
-    seat_number VARCHAR(100) not null;
-    reservation_name DATETIME not null;
-
+    id INT AUTO_INCREMENT primary key,
+    customer_name VARCHAR(100) not null,
+    movie_name VARCHAR(100) not null,
+    seat_number VARCHAR(100) not null,
+    reservation_name DATETIME not null
 )
 
 
